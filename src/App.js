dce6d688c641669e084ff815b403366cddf2e0c2
Suryzw <139625661+Suryzw@users.noreply.github.com>
2023-12-11 22:24:47 +0700

    nambah

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/landingPage/Header'
+import Home from './components/landingPage/Home'
+import AboutUs from './components/landingPage/AboutUs'
+import Login from './components/auth/Login'
+import SignUp from './components/auth/SignUp'
+import { HashRouter, Routes, Route,} from "react-router-dom"
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HashRouter>
+      <Navbar/>
+        <Routes>
+          <Route index element={<Home />}/>
+          <Route path='/home' element={<Home/>}/>
+          <Route path="/about" element={<AboutUs />} />
+          <Route path='/auth/login' element={<Login/>}/>
+          <Route path='/auth/signup' element={<SignUp/>}/>
+        </Routes>
+      </HashRouter>
+    </>
+    
   );
 }
 
---
+++ src/components/landingPage/AboutUs.js
@@ -0,0 +1,11 @@
+import Header from './Header';
+
+function AboutUs(){
+    return(
+        <>
+        
+        <h1>This is About Us</h1>
+        </>
+    )
+}
+export default AboutUs;
